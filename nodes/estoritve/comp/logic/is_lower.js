6c370072031316282c3367911317749a07eb4952
Timotej Gale <timotej.gale@gmail.com>
2015-07-10 16:16:37 +0200

    Added basic conditional nodes (greater, lower, equal)

---
+++ nodes/estoritve/comp/logic/is_lower.js
@@ -0,0 +1,27 @@
+module.exports = function(RED) {
+	function isLowerNode(config) {
+		RED.nodes.createNode(this, config);
+		this.compValue = config.compValue;
+		var node = this;
+		
+		this.on('input', function(msg) {
+			if(!node.compValue) {
+				node.warn("Ni vhodnega podatka!", msg);
+				return;
+			}
+			if(isNaN(node.compValue)) {
+				node.warn("Vhodni podatek ni število!", msg);
+				return;
+			}
+			var value = msg.value || msg.payload.value;
+			if(parseFloat(value) < parseFloat(node.compValue))
+				msg.state = true;
+			else
+				msg.state = false;
+			
+			msg.value = value;
+			node.send(msg);
+		});
+	}
+	RED.nodes.registerType("jeManjse", isLowerNode);
+}
